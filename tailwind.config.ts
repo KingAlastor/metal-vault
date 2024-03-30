34f8220808e048f035b9d111b689ef0c50a3bc31
KingAlastor <152447707+KingAlastor@users.noreply.github.com>
2024-03-30 14:12:39 +0200

    initial commit, new project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,15 +6,52 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        lightTheme: {
+          primary: '#4C51BF',
+          'primary-focus': '#434190',
+          'primary-content': '#ffffff',
+          secondary: '#f000b8',
+          'secondary-focus': '#bd0091',
+          'secondary-content': '#ffffff',
+          accent: '#37cdbe',
+          'accent-focus': '#2aa79b',
+          'accent-content': '#ffffff',
+          neutral: '#3d4451',
+          'neutral-focus': '#2a2e37',
+          'neutral-content': '#ffffff',
+          base: '#ffffff',
+          'base-content': '#1f2937',
+          info: '#2094f3',
+          success: '#009485',
+          warning: '#ff9900',
+          error: '#ff5724',
+        },
+        darkTheme: {
+          primary: '#5861b3',
+          'primary-focus': '#434190',
+          'primary-content': '#ffffff',
+          secondary: '#f000b8',
+          'secondary-focus': '#bd0091',
+          'secondary-content': '#ffffff',
+          accent: '#37cdbe',
+          'accent-focus': '#2aa79b',
+          'accent-content': '#ffffff',
+          neutral: '#16181D',
+          'neutral-focus': '#1a202c',
+          'neutral-content': '#ffffff',
+          base: '#1a202c',
+          'base-content': '#ffffff',
+          info: '#2094f3',
+          success: '#009485',
+          warning: '#ff9900',
+          error: '#ff5724',        
+        },
       },
-    },
+    ],
   },
-  plugins: [],
 };
 export default config;
