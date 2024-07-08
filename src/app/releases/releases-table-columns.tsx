49b5d0023ef6704f6a4c5086af3d73814cb17590
KingAlastor <152447707+KingAlastor@users.noreply.github.com>
2024-07-08 17:10:13 +0300

    added row actions

--- src/app/releases/releases-table-columns.tsx
+++ src/app/releases/releases-table-columns.tsx
@@ -1,6 +1,17 @@
 "use client"
 
 import { ColumnDef } from "@tanstack/react-table"
+import { MoreHorizontal } from "lucide-react"
+ 
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 export type BandAlbum = {
   bandId: string;
@@ -26,6 +37,31 @@ export const columns: ColumnDef<BandAlbum>[] = [
       const dateFull: string = row.getValue("releaseDate");
       const dateFormatted: string = new Date(dateFull).toLocaleDateString("en-GB");
       return <div>{dateFormatted}</div>;
+    },
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const bandAlbum = row.original
+ 
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(bandAlbum.bandId)}
+            >
+              Copy Band Id
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )
     },
   },
 ]
