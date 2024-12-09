6701939a8d14945df4a51c1bc789c5081d5f7192
KingAlastor <152447707+KingAlastor@users.noreply.github.com>
2024-12-09 21:52:57 +0200

    added ReactQueryProvider to project

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -13,15 +13,15 @@ export default async function Layout({
 
   return (
     <SessionProvider session={session}>
-        <div className="flex min-h-screen flex-col">
-          <NavBar />
-          <div className="mx-auto flex w-full max-w-7xl grow gap-5 p-5">
-            <MenuBar className="sticky top-[5.25rem] hidden h-fit flex-none space-y-3 rounded-2xl bg-card px-3 py-5 shadow-sm sm:block lg:px-5 xl:w-80" />
-            <div className="flex-1">{children}</div>
-            <PromotionsBar className="sticky top-[5.25rem] hidden sm:block w-full max-w-xs" />
-          </div>
-          <MenuBar className="sticky bottom-0 flex w-full justify-center gap-5 border-t bg-card p-3 sm:hidden" />
+      <div className="flex min-h-screen flex-col">
+        <NavBar />
+        <div className="mx-auto flex w-full max-w-7xl grow gap-5 p-5">
+          <MenuBar className="sticky top-[5.25rem] hidden h-fit flex-none space-y-3 rounded-2xl bg-card px-3 py-5 shadow-sm sm:block lg:px-5 xl:w-80" />
+          <div className="flex-1">{children}</div>
+          <PromotionsBar className="sticky top-[5.25rem] hidden sm:block w-full max-w-xs" />
         </div>
+        <MenuBar className="sticky bottom-0 flex w-full justify-center gap-5 border-t bg-card p-3 sm:hidden" />
+      </div>
     </SessionProvider>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
+import ReactQueryProvider from "./ReactQueryProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,14 +19,16 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
+        <ReactQueryProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </ReactQueryProvider>
       </body>
     </html>
   );
