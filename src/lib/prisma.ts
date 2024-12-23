528521faf062afc6c8fb47a1472d42db6d4c571c
KingAlastor <152447707+KingAlastor@users.noreply.github.com>
2024-12-23 20:22:27 +0200

    modified prisma client to use just one client in dev environment

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,10 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ['query'],
+  })
 
-//global is not affected by hot reload
-export const prisma = globalThis.prisma || new PrismaClient();
-
-export default prisma;
-
-//if we're in development, let's set the global prisma object
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
-
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
