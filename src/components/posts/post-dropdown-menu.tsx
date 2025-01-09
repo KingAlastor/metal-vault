9c3d4c81e0b41eb0017c04cf515981781962c3d0
KingAlastor <152447707+KingAlastor@users.noreply.github.com>
2025-01-09 21:31:02 +0200

    moved dropdown menu separator for delete post into its own if block

--- src/components/posts/post-dropdown-menu.tsx
+++ src/components/posts/post-dropdown-menu.tsx
@@ -42,11 +42,13 @@ const PostDropdownMenu = ({ post }: PostDropdownMenuProps) => {
           <DropdownMenuItem onClick={handleAddToFavoritesClick}>
             <div className="dropdown-options">Report Post</div>
           </DropdownMenuItem>
-          <DropdownMenuSeparator />
           {status === "authenticated" && post.userId === data.user.id && (
-            <DropdownMenuItem onClick={() => setIsDialogOpen(true)}>
-              <div className="dropdown-options">Delete Post</div>
-            </DropdownMenuItem>
+            <>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem onClick={() => setIsDialogOpen(true)}>
+                <div className="dropdown-options">Delete Post</div>
+              </DropdownMenuItem>
+            </>
           )}
         </DropdownMenuContent>
       </DropdownMenu>
