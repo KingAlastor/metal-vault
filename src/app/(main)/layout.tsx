cd11b34179eeeb5b8927ff50694a4e7f4ef47c59
KingAlastor <152447707+KingAlastor@users.noreply.github.com>
2025-01-26 19:40:39 +0200

    add Toaster component to layout for notifications

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -3,6 +3,7 @@ import getSession from "@/lib/auth/getSession";
 import MenuBar from "./MenuBar";
 import { SessionProvider } from "next-auth/react";
 import PromotionsBar from "./PromotionsBar";
+import { Toaster } from "@/components/ui/toaster";
 
 export default async function Layout({
   children,
@@ -18,6 +19,7 @@ export default async function Layout({
         <div className="mx-auto flex w-full max-w-7xl grow gap-5 p-5">
           <MenuBar className="sticky top-[5.25rem] hidden h-fit flex-none space-y-3 rounded-2xl bg-card px-3 py-5 shadow-sm sm:block lg:px-5 xl:w-80" />
           <div className="flex-1">{children}</div>
+          <Toaster />
           <PromotionsBar className="sticky top-[5.25rem] hidden sm:block w-full max-w-xs" />
         </div>
         <MenuBar className="sticky bottom-0 flex w-full justify-center gap-5 border-t bg-card p-3 sm:hidden" />
