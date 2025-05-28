3597cb6332aa95b01d4bcaa3400bf5dca6c4b89a
KingAlastor <152447707+KingAlastor@users.noreply.github.com>
2025-05-28 19:44:53 +0300

    fix: Adjust layout spacing and visibility for MenuBar and PromotionsBar components

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -11,13 +11,13 @@ export default async function Layout({
   return (
     <div className="flex min-h-screen flex-col">
       <NavBar />
-      <div className="mx-auto flex w-full max-w-7xl grow gap-5 p-5">
-        <MenuBar className="sticky top-[5.25rem] hidden h-fit flex-none space-y-3 rounded-2xl bg-card px-3 py-5 shadow-sm sm:block lg:px-5 xl:w-80" />
-        <div className="flex-1">{children}</div>
+      <div className="mx-auto flex w-full max-w-7xl grow gap-0 lg:gap-5 p-0 lg:p-5">
+        <MenuBar className="sticky top-[5.25rem] hidden h-fit flex-none space-y-3 rounded-2xl bg-card px-3 py-5 shadow-sm lg:block lg:px-5 xl:w-80" />
+        <div className="flex-1 min-w-0 w-full">{children}</div>
         <Toaster />
-        <PromotionsBar className="sticky top-[5.25rem] hidden sm:block w-full max-w-xs" />
+        <PromotionsBar className="sticky top-[5.25rem] hidden lg:block w-full max-w-xs" />
       </div>
-      <MenuBar className="sticky bottom-0 flex w-full justify-center gap-5 border-t bg-card p-3 sm:hidden" />
+      <MenuBar className="sticky bottom-0 flex w-full justify-center gap-5 border-t bg-card p-3 lg:hidden" />
     </div>
   );
 }
