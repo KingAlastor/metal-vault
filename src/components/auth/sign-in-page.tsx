4b78551f63cc7947eb2661e2c72c15016158ba5b
KingAlastor <152447707+KingAlastor@users.noreply.github.com>
2025-07-15 19:20:21 +0300

    fix: Refactor sign-in page layout and improve structure

--- src/components/auth/sign-in-page.tsx
+++ src/components/auth/sign-in-page.tsx
@@ -1,14 +1,14 @@
-'use client';
+"use client";
 import { GoogleLogin } from "@/components/auth/google-login";
 import { SpotifyLogin } from "@/components/auth/spotify-login";
 
 export default function CustomOAuthSignIn() {
-
   return (
-    <div>
-      <h1>My Custom Sign In page</h1>
-      <GoogleLogin />
-      <SpotifyLogin />
+    <div className="flex flex-col items-center justify-center min-h-screenp-4">
+      <div className="space-y-4">
+        <GoogleLogin />
+        <SpotifyLogin />
+      </div>
     </div>
   );
 }
