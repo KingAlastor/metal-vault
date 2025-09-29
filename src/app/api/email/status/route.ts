590f886a67a50bffb6c11d200ed9825ac95f1856
KingAlastor <152447707+KingAlastor@users.noreply.github.com>
2025-09-30 01:36:01 +0300

    fix: trim token extraction from authorization header

--- src/app/api/email/status/route.ts
+++ src/app/api/email/status/route.ts
@@ -3,14 +3,9 @@ import { fetchEnvironmentVariables } from "@/lib/general/env-variables";
 
 export async function POST(req: Request) {
   const header = req.headers.get("authorization");
-  const token = header?.split("Bearer")[1];
+  const token = header?.replace("Bearer", "").trim();
   const bearerToken = await fetchEnvironmentVariables("BEARER_TOKEN");
-  console.log(
-    "Incoming Lambda function token: ",
-    token,
-    " Local token: ",
-    bearerToken
-  );
+
   if (!token || token != bearerToken) {
     return new Response("Unauthorized", { status: 401 });
   }
