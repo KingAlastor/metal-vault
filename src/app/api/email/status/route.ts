68d10fd3241ffb7d096225fd8854a15ba7d3b86d
KingAlastor <152447707+KingAlastor@users.noreply.github.com>
2025-09-30 01:23:44 +0300

    fix: update bearer token retrieval to use fetchEnvironmentVariables for consistency

--- src/app/api/email/status/route.ts
+++ src/app/api/email/status/route.ts
@@ -1,19 +1,25 @@
 import { updateEmailAddressStatus } from "@/lib/data/user-email-settings-data";
+import { fetchEnvironmentVariables } from "@/lib/general/env-variables";
 
 export async function POST(req: Request) {
   const header = req.headers.get("authorization");
   const token = header?.split("Bearer")[1];
-  const bearerToken = process.env.BEARER_TOKEN;
-
+  const bearerToken = await fetchEnvironmentVariables("BEARER_TOKEN");
+  console.log(
+    "Incoming Lambda function token: ",
+    token,
+    " Local token: ",
+    bearerToken
+  );
   if (!token || token != bearerToken) {
     return new Response("Unauthorized", { status: 401 });
   }
 
   const body = await req.json();
-
+  console.log("body: ", body);
   if (body.email && body.status) {
     await updateEmailAddressStatus(body.email, body.status.toUpperCase());
   }
 
-  return Response.json({status: true})
+  return Response.json({ status: true });
 }
