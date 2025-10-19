8e003d73c8b86a8142700c26367a3bc7bc75a2b7
KingAlastor <152447707+KingAlastor@users.noreply.github.com>
2025-10-19 14:37:05 +0300

    fix: adjust padding in NavBar for improved layout

--- src/app/(main)/NavBar.tsx
+++ src/app/(main)/NavBar.tsx
@@ -12,7 +12,7 @@ export default function NavBar() {
 
   return (
     <header className="sticky top-0 z-10 bg-card shadow-sm">
-      <div className="mx-auto flex max-w-7xl flex-wrap items-center justify-between gap-5 px-5 py-3">
+      <div className="mx-auto flex max-w-7xl flex-wrap items-center justify-between gap-5 px-1.5 py-1.5">
         <Link
           href="/"
           className="flex items-center text-2xl font-bold text-primary"
